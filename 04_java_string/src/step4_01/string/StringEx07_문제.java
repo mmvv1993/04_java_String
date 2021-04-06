964737d56e982da61fd358c29a06c0d9547163c6
user <user@401-23>
2021-04-06 17:30:39 +0900

    4/6 화요일 ==오늘 드리는 예제는 더이상 없습니다. 반복해서 풀어보기(감각,경험,노하우키우기)

---
+++ 04_java_string/src/step4_01/string/StringEx07_문제.java
@@ -0,0 +1,29 @@
+package step4_01.string;
+
+import java.util.Arrays;
+
+public class StringEx07_문제 {
+
+	public static void main(String[] args) {
+		
+		// 문제) 이름은 name배열에 성적은 score배열에 각각 저장 및 출력
+		
+		String str = "김철수/87,이만수/42,이영희/95";
+		
+		String[] name = new String[3];
+		int[] score = new int[3];
+				
+		String[] temp1 = str.split(",");		// 김철수/87이만수/42이영희/95
+		
+		for(int i=0; i<temp1.length; i++) {
+			String[] temp2 = temp1[i].split("/");		//김철수,87,이만수,42,이영희,95
+			name[i] = temp2[0];
+			score[i] = Integer.parseInt(temp2[1]);
+		}
+		
+		System.out.println(Arrays.toString(name));
+		System.out.println(Arrays.toString(score));
+	}
+	
+
+}
