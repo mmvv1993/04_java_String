bc3b190eceaf0938145e1e62294148945e14c510
user <user@401-23>
2021-04-08 15:21:54 +0900

    아스키 // 문자 > 숫자 65 : 'A'  , 97 : 'a ' 상식적으로암기

--- 04_java_string/src/step4_01/string/StringEx04.java
+++ 04_java_string/src/step4_01/string/StringEx04.java
@@ -36,6 +36,31 @@ public class StringEx04 {
 		// 2-3) String.valueOf(숫자) 사용
 		String str3 = String.valueOf(nNum);
 		System.out.println(str3 + 1); // 101
+		System.out.println("=================");
+		
+		
+		//참고 ) 구글에서 ASCII 코드표 검색 ( 65 : 'A' , 97 : 'a' 상식으로 암기)
+		
+		//문자 > 숫자
+		char ch1 = 'a';
+		int n1 = (int)ch1;
+		System.out.println(ch1+" / " + n1);
+		
+		char ch2 = 'A';
+		int n2 = (int)ch2;
+		System.out.println(ch2 + " / " + n2);
+		
+		//숫자 > 문자 
+		int n3 = 100;
+		char ch3 = (char)n3;
+		System.out.println(n3 + " / " + ch3);
+		System.out.println();
+		
+		System.out.println((int)'s'+ 3);   //115+3
+		System.out.println((char)(n3+3));	//d + 3 == g
+		
+		
+		
 		
 	}
 
